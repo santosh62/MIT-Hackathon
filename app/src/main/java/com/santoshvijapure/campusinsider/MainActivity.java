b819bd66d5895d9bf295b34b85df2d204eb9fa90
Prathamesh More <moreprathameshmore@hotmail.com>
2018-02-16 14:07:47 +0530

    Added Navigation Header

--- app/src/main/java/com/santoshvijapure/campusinsider/MainActivity.java
+++ app/src/main/java/com/santoshvijapure/campusinsider/MainActivity.java
@@ -1,5 +1,6 @@
 package com.santoshvijapure.campusinsider;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+
+
+
+                return false;
+            }
+        });
+
 
     }
 
